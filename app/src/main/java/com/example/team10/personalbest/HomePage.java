20e044498c75a3d2695865d567e3acecd793456d
Daniel Harnanto <dharnant@ucsd.edu>
2019-02-07 17:10:56 -0800

    Added shared preferences for currentGoal

--- app/src/main/java/com/example/team10/personalbest/HomePage.java
+++ app/src/main/java/com/example/team10/personalbest/HomePage.java
@@ -2,9 +2,11 @@ package com.example.team10.personalbest;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -16,6 +18,7 @@ public class HomePage extends AppCompatActivity {
 
     private int currentGoal = 5000;
     private int stepCount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +50,13 @@ public class HomePage extends AppCompatActivity {
         });
 
         openCongratsDialog();
+
+
+        SharedPreferences goalPreferences = getSharedPreferences("goal_count", MODE_PRIVATE);
+
+        int updatedGoal = goalPreferences.getInt("goalCount", 5000);
+        goal_text.setText(Integer.toString(updatedGoal));
+
     }
 
     public void launchRunning() {
@@ -83,14 +93,22 @@ public class HomePage extends AppCompatActivity {
     }
 
     public void openNewGoalDialog() {
+        SharedPreferences goalPreferences = getSharedPreferences("goal_count", MODE_PRIVATE);
+
         AlertDialog.Builder newGoalBuilder = new AlertDialog.Builder(this);
         newGoalBuilder.setMessage(R.string.set_method);
-        newGoalBuilder.setPositiveButton(getString(R.string.suggested) + " " + Integer.toString(currentGoal + 500), new DialogInterface.OnClickListener() {
+        newGoalBuilder.setPositiveButton(getString(R.string.suggested) + " " + Integer.toString(goalPreferences.getInt("goalCount", 5000) + 500), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                currentGoal = currentGoal + 500;
+                SharedPreferences goalPreferences = getSharedPreferences("goal_count", MODE_PRIVATE);
+                SharedPreferences.Editor editor = goalPreferences.edit();
+                currentGoal = goalPreferences.getInt("goalCount", 5000) + 500;
                 TextView goal_text = findViewById(R.id.currentGoal);
                 goal_text.setText(Integer.toString(currentGoal));
+
+                editor.putInt("goalCount", Integer.parseInt(goal_text.getText().toString()));
+                editor.apply();
+
                 Toast.makeText(HomePage.this, R.string.goal_updated_toast, Toast.LENGTH_LONG).show();
             }
         });
