be0b19dd4fdc9324b2abeb151355c797a3476642
moxyang18 <moy004@ucsd.edu>
2019-02-10 13:00:30 -0800

    UI page with the barchart shown
    After pressing barchart button

---
+++ app/src/main/java/com/example/team10/personalbest/BarChartActivity.java
@@ -0,0 +1,123 @@
+package com.example.team10.personalbest;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+
+public class BarChartActivity extends AppCompatActivity {
+
+    private BarChart barChart10;
+    private ArrayList<ArrayList> stepList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_chart);
+        config();
+
+        barChart10 = (BarChart) findViewById(R.id.bar_chart);
+        /* first load the steps in stepList, which will store an array of lists each containing the
+           planned steps and unplanned steps
+        stepList = xxx;*/
+        displayChart();
+
+    }
+
+    private void displayChart() {
+         /* first load the steps in stepList, which will store an array of lists each containing the
+           planned steps and unplanned steps
+        stepList = xxx;
+        int xInd = 2;
+        for (ArrayList<int> day: stepList)
+            entries.add(new BarEntry(xInd++, new float[]{day[0], day[1]}));
+        */
+
+        // create entries of each day's steps of the week
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, new float[]{1000, 200}));
+        entries.add(new BarEntry(1, new float[]{2060, 1000}));
+        entries.add(new BarEntry(2, new float[]{1000, 200}));
+        entries.add(new BarEntry(3, new float[]{2000, 1000}));
+        entries.add(new BarEntry(4, new float[]{2000, 1000}));
+        entries.add(new BarEntry(5, new float[]{500, 5000}));
+        entries.add(new BarEntry(6, new float[]{700, 1400}));
+
+        BarDataSet entrySet = new BarDataSet(entries, ""); //"DailySteps");
+
+        // set the different colors for the vertical bars
+        entrySet.setColors(this.getColors());
+        entrySet.setBarShadowColor(Color.GRAY);
+
+        // display the bar values, and mark each color with corresponding meaning
+        entrySet.setDrawValues(true);
+        entrySet.setStackLabels(new String[]{"UnplannedSteps", "PlannedSteps"});
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(entrySet);
+
+        BarData data = new BarData(dataSets);
+        //data.setValueFormatter(new MyValueFormatter());
+        data.setValueTextColor(Color.BLACK);
+
+        // create labels representing the x-axis days
+        ArrayList<String> labels = new ArrayList<>();
+        //String curDay;
+        //(planned steps != 0) as condition
+        labels.add((true)? "Sun(✓)":"Sun");
+        labels.add((true)? "Mon(✓)":"Mon");
+        labels.add((false)? "Tue(✓)":"Tue");
+        labels.add((true)? "Wed(✓)":"Wed");
+        labels.add((true)? "Thu(✓)":"Thu");
+        labels.add((false)? "Fri(✓)":"Fri");
+        labels.add((true)? "Sat(✓)":"Sat");
+
+        XAxis x = barChart10.getXAxis();
+        x.setValueFormatter(new IndexAxisValueFormatter(labels));
+        //x.setCenterAxisLabels(true);
+        x.setPosition(XAxis.XAxisPosition.BOTTOM);
+        x.setGranularity(1);
+        x.setGranularityEnabled(true);
+
+        //barChart10.setDragEnabled(true);
+
+        // initialize the BarData with the entries and the labels
+        //BarData stepData = new BarData(entrySet);
+
+        // put data onto the bar chart
+        barChart10.setData(data); //stepData);
+        //barChart10.setFitBars(true);
+        barChart10.animateY(1000);
+        barChart10.invalidate();
+    }
+
+    private int[] getColors() {
+        int stackSize = 2;
+        // set 2 colors for unplanned and planned steps
+        int[] colors = new int[stackSize];
+        colors[0] = Color.parseColor("#2cb5d9");
+        colors[1] = Color.parseColor("#d34a26");
+        return colors;
+    }
+
+    private void config() {
+        Button back_button = findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/team10/personalbest/HomePage.java
@@ -0,0 +1,187 @@
+package com.example.team10.personalbest;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HomePage extends AppCompatActivity {
+
+    private int currentGoal = 5000;
+    private int stepCount = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        TextView goal_text = findViewById(R.id.currentGoal);
+        goal_text.setText(Integer.toString(currentGoal));
+
+        TextView step_text = findViewById(R.id.stepsCount);
+        step_text.setText(Integer.toString(stepCount));
+
+        Button run_button = findViewById(R.id.startButton);
+        run_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                launchRunning();
+            }
+        });
+
+        ImageButton bar_chart_button = findViewById(R.id.barButton);
+        bar_chart_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                launchBarChart();
+            }
+        });
+
+        openCongratsDialog();
+
+
+        SharedPreferences goalPreferences = getSharedPreferences("goal_count", MODE_PRIVATE);
+
+        int updatedGoal = goalPreferences.getInt("goalCount", 5000);
+        goal_text.setText(Integer.toString(updatedGoal));
+
+    }
+
+    public void launchRunning() {
+        Intent intent = new Intent(this, RunningMode.class);
+        startActivity(intent);
+    }
+
+    public void launchBarChart() {
+        Intent intent = new Intent(this, BarChartActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCongratsDialog() {
+        AlertDialog.Builder congratsBuilder = new AlertDialog.Builder(this);
+        congratsBuilder.setMessage(R.string.congrats_message);
+        congratsBuilder.setPositiveButton(R.string.yes_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                openNewGoalDialog();
+            }
+        });
+
+        congratsBuilder.setNegativeButton(R.string.not_now, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog congratsDialog = congratsBuilder.create();
+        congratsDialog.setCanceledOnTouchOutside(false);
+        //if(currentGoal == stepCount) {
+            congratsDialog.show();
+        //}
+    }
+
+    public void openNewGoalDialog() {
+        SharedPreferences goalPreferences = getSharedPreferences("goal_count", MODE_PRIVATE);
+
+        AlertDialog.Builder newGoalBuilder = new AlertDialog.Builder(this);
+        newGoalBuilder.setMessage(R.string.set_method);
+        newGoalBuilder.setPositiveButton(getString(R.string.suggested) + " " + Integer.toString(goalPreferences.getInt("goalCount", 5000) + 500), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SharedPreferences goalPreferences = getSharedPreferences("goal_count", MODE_PRIVATE);
+                SharedPreferences.Editor editor = goalPreferences.edit();
+                currentGoal = goalPreferences.getInt("goalCount", 5000) + 500;
+                TextView goal_text = findViewById(R.id.currentGoal);
+                goal_text.setText(Integer.toString(currentGoal));
+
+                editor.putInt("goalCount", Integer.parseInt(goal_text.getText().toString()));
+                editor.apply();
+
+                Toast.makeText(HomePage.this, R.string.goal_updated_toast, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        newGoalBuilder.setNegativeButton(R.string.custom, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //add custom goal functionality later
+                openCustomDialog();
+            }
+        });
+        AlertDialog newGoalDialog = newGoalBuilder.create();
+        newGoalDialog.setCanceledOnTouchOutside(false);
+        newGoalDialog.show();
+    }
+
+    public void openCustomDialog() {
+        AlertDialog.Builder customBuilder = new AlertDialog.Builder(this);
+        customBuilder.setMessage(R.string.custom_prompt);
+
+        final EditText customField = new EditText(this);
+        customField.setInputType(InputType.TYPE_CLASS_NUMBER);
+        customField.setFilters(new InputFilter[]{new InputFilter.LengthFilter(6)});
+
+
+        customBuilder.setView(customField);
+        customBuilder.setPositiveButton(R.string.confirm_custom, null);
+
+        customBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {}
+        });
+        final AlertDialog customDialog = customBuilder.create();
+        customDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                Button confirmButton = customDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                confirmButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        SharedPreferences goalPreferences = getSharedPreferences("goal_count", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = goalPreferences.edit();
+                        TextView goal_text = findViewById(R.id.currentGoal);
+
+                        String customInput = customField.getText().toString();
+                        int tooLow = 0;
+
+                        if(!customInput.isEmpty()) {
+                            tooLow = Integer.parseInt(customInput);
+                        }
+                        if(!customInput.isEmpty() && tooLow < 5000) {
+                            Toast.makeText(HomePage.this, R.string.too_low, Toast.LENGTH_LONG).show();
+                        }
+                        if(customInput.isEmpty()) {
+                            Toast.makeText(HomePage.this, R.string.custom_empty_prompt, Toast.LENGTH_LONG).show();
+                        }
+                        if(!customInput.isEmpty() && tooLow >= 5000){
+                            goal_text.setText(customField.getText());
+
+                            editor.putInt("goalCount", Integer.parseInt(goal_text.getText().toString()));
+                            editor.apply();
+
+                            Toast.makeText(HomePage.this, R.string.goal_updated_toast, Toast.LENGTH_LONG).show();
+
+                            customDialog.dismiss();
+                        }
+                    }
+                });
+            }
+        });
+        customDialog.setCanceledOnTouchOutside(false);
+        customDialog.show();
+    }
+
+}
