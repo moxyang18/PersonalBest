84eccfe4be867cf35a4764d1c2ceae2c88b14a5d
moxyang18 <moy004@ucsd.edu>
2019-03-09 11:22:29 -0800

    Firebase app initialization (#9, #11, #70)

--- app/src/main/java/com/example/team10/personalbest/HomePage.java
+++ app/src/main/java/com/example/team10/personalbest/HomePage.java
@@ -33,6 +33,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -68,6 +69,9 @@ public class HomePage extends AppCompatActivity{
 
         // Add factory to have mock mediator
         activityMediator = new ActivityMediator(this);
+
+        // Init app
+        FirebaseApp.initializeApp(this);
 
         // Get the shared instance for firebase
         firebaseAuth = FirebaseAuth.getInstance();
